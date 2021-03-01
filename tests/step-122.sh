6021dd8135bf42d70676ba6ddc1ce91803fd69b3
Gabriel Nützi <gabriel.nuetzi@cyfex.com>
2021-03-01 19:15:44 +0100

    Improve test 122

--- tests/step-122.sh
+++ tests/step-122.sh
@@ -7,21 +7,21 @@
 DIALOG=$(git config --global githooks.dialog)
 
 if [ ! -f "$DIALOG" ]; then
-    echo "! Dialog tool '$DIALOG' not found." >&2
+    echo "! Dialog tool '$DIALOG' not found."
     exit 2
 fi
 
 if ! "$DIALOG" --version >/dev/null 2>&1; then
-    echo "! Dialog tool not working properly." >&2
+    echo "! Dialog tool not working properly."
     exit 3
 fi
 
 if ! "$DIALOG" --version 2>&1 | grep -q "dialog version"; then
-    echo "! Dialog tool not working properly" >&2
-    exit 3
+    echo "! Dialog tool not working properly"
+    exit 4
 fi
 
 if ! "$DIALOG" options --help 2>&1 | grep -q "dialog"; then
-    echo "! Dialog tool not working properly" >&2
-    exit 3
+    echo "! Dialog tool not working properly"
+    exit 5
 fi
