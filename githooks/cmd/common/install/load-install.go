1fdaa946c30c72b222c5e0325404e6213dd9732c
Gabriel Nützi <gnuetzi@gmail.com>
2021-05-31 18:06:25 +0200

    Fix wrong output if install directory is empty :anchor:

--- githooks/cmd/common/install/load-install.go
+++ githooks/cmd/common/install/load-install.go
@@ -3,22 +3,27 @@ package install
 import (
 	cm "gabyx/githooks/common"
 	"gabyx/githooks/hooks"
+	strs "gabyx/githooks/strings"
 	"path"
 	"path/filepath"
 
 	"github.com/mitchellh/go-homedir"
 )
 
-// LoadInstallDir loads the install directory and uses a default it
+// LoadInstallDir loads the install directory and uses a default if
 // it does not exist.
 func LoadInstallDir(log cm.ILogContext) (installDir string) {
 
 	installDir = hooks.GetInstallDir()
 
 	if !cm.IsDirectory(installDir) {
-		log.WarnF("Install directory '%s' does not exist.\n"+
-			"Githooks installation is corrupt!\n"+
-			"Using default location '~/.githooks'.", installDir)
+
+		if strs.IsNotEmpty(installDir) {
+			log.WarnF("Install directory '%s' does not exist.\n"+
+				"Githooks installation is corrupt!\n"+
+				"Using default location '~/.githooks'.", installDir)
+		}
+
 		home, err := homedir.Dir()
 		cm.AssertNoErrorPanic(err, "Could not get home directory.")
 		installDir = path.Join(filepath.ToSlash(home), hooks.HooksDirName)
