689a010d607fbaa48b1a61f2bfcede0de496d8e0
Gabriel Nützi <gnuetzi@gmail.com>
2023-05-21 14:47:00 +0200

    feat: Added images config file :anchor:

--- githooks/hooks/images_test.go
+++ githooks/hooks/images_test.go
@@ -21,10 +21,23 @@ func TestLoadImagesConfig(t *testing.T) {
 
 	err = cm.StoreYAML(file.Name(), c)
 	cm.AssertNoErrorPanic(err, "Could not store yaml.")
-
 	config, err := loadImagesConfigFile(file.Name())
 	cm.AssertNoErrorPanic(err, "Could not load yaml.")
 
 	assert.Equal(t, config.Images["bla"].Build.File, "thisfile")
 	assert.Nil(t, config.Images["bla"].Pull)
+
+	c = createImageConfigFile()
+	cc = ImageConfig{}
+	cc.Pull = &ImageConfigPull{Name: "container", Tag: "1.2.9", Digest: "sha256:123"}
+	cc.Build = &ImageConfigBuild{File: "thisfile", Target: "stage-1"}
+	c.Images["bla"] = cc
+
+	err = cm.StoreYAML(file.Name(), c)
+	cm.AssertNoErrorPanic(err, "Could not store yaml.")
+	config, err = loadImagesConfigFile(file.Name())
+	cm.AssertNoErrorPanic(err, "Could not load yaml.")
+
+	assert.Equal(t, config.Images["bla"].Build.File, "thisfile")
+	assert.Equal(t, config.Images["bla"].Pull.Name, "container")
 }
