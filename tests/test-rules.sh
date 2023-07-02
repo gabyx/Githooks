1e451f44f7793d518d46d28393838688f6051d97
Gabriel Nützi <gnuetzi@gmail.com>
2023-07-02 14:25:55 +0200

    fix: Correct rules test to run all hooks over all repo :anchor:

--- tests/test-rules.sh
+++ tests/test-rules.sh
@@ -8,27 +8,35 @@ rootDir=$(git rev-parse --show-toplevel)
 cat <<EOF | docker build --force-rm -t githooks:test-rules -
 FROM golang:1.20-alpine
 RUN apk add git curl git-lfs --update-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/main --allow-untrusted
-RUN apk add bash jq
+RUN apk add bash jq docker
 
 RUN git config --global safe.directory /data
 
 # CVE https://github.blog/2022-10-18-git-security-vulnerabilities-announced/#cve-2022-39253
 RUN git config --system protocol.file.allow always
 
-RUN curl -fsSL https://github.com/mvdan/sh/releases/download/v3.1.1/shfmt_v3.1.1_linux_amd64 -o /usr/local/bin/shfmt \
-    && chmod +x /usr/local/bin/shfmt \
-    && shfmt --version
-RUN T=$(mktemp); curl -fsSL https://github.com/koalaman/shellcheck/releases/download/v0.7.1/shellcheck-v0.7.1.linux.x86_64.tar.xz -o "\\\$T" \
-    && tar -xf "\\\$T" --strip-components=1 -C /usr/local/bin/ \
-    && chmod +x /usr/local/bin/shellcheck \
-    && shellcheck --version
-
 RUN curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b \$(go env GOPATH)/bin v1.52.2
+
+# Install Githooks
+RUN temp=\$(mktemp -d) && \
+    curl -sL "https://github.com/gabyx/Githooks/releases/download/v2.3.3/githooks-2.3.3-linux.amd64.tar.gz" \
+        -o "\$temp/githooks.tar.gz" && \
+        tar -C "\$temp" -xf "\$temp/githooks.tar.gz" && \
+        "\$temp/cli" installer --non-interactive && \
+        rm -rf "\$tempDir"
+
+RUN git config --global user.email "githook@test.com" && \
+    git config --global user.name "Githook Tests" && \
+    git config --global init.defaultBranch main && \
+    git config --global core.autocrlf false
 
 ENV DOCKER_RUNNING=true
 EOF
 
-if ! docker run --rm -it -v "$rootDir":/data -w /data githooks:test-rules tests/exec-rules.sh; then
+if ! docker run --rm -it \
+    -v "$rootDir":/data \
+    -v "/var/run/docker.sock:/var/run/docker.sock" \
+    -w /data githooks:test-rules tests/exec-rules.sh; then
     echo "! Check rules had failures."
     exit 1
 fi
