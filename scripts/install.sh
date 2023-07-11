7e2363388853767cb9d9419ab0076de7212a8d90
Nuetzi, Gabriel <gabriel.nuetzi@dentsplysirona.com>
2023-07-11 12:40:19 +0200

    fix: Improve install script for platform Windows :anchor:

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,103 @@ function checkTool() {
     fi
 }
 
+function getPlatformOS() {
+    local -n _platformOS="$1"
+    local platformOSDist=""
+    local platformOSVersion=""
+
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        _platformOS="linux"
+    elif [[ "$OSTYPE" == "linux-musl"* ]]; then
+        # Alpine linux
+        _platformOS="linux"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        _platformOS="darwin"
+    elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        _platformOS="freebsd"
+    else
+        # Resort to `uname` for windows stuff.
+        local name
+        name=$(uname -a)
+        case "$name" in
+        CYGWIN*) _platformOS="windows" && platformOSDist="cygwin" ;;
+        MINGW*) _platformOS="windows" && platformOSDist="mingw" ;;
+        *Msys) _platformOS="windows" && platformOSDist="msys" ;;
+        *) die "Platform: '$name' not supported." ;;
+        esac
+    fi
+
+    if [ "$_platformOS" = "linux" ]; then
+
+        if [ "$(lsb_release -si 2>/dev/null)" = "Ubuntu" ]; then
+            platformOSDist="ubuntu"
+            platformOSVersion=$(grep -m 1 "VERSION_CODENAME=" "/etc/os-release" |
+                sed -E "s|.*=[\"']?(.*)[\"']?|\1|")
+        elif grep -qE 'ID="?debian' "/etc/os-release"; then
+            platformOSDist="debian"
+            platformOSVersion=$(grep -m 1 "VERSION_CODENAME=" "/etc/os-release" |
+                sed -E "s|.*=[\"']?(.*)[\"']?|\1|")
+        elif grep -qE 'ID="?alpine' "/etc/os-release"; then
+            platformOSDist="alpine"
+            platformOSVersion=$(grep -m 1 "VERSION_ID=" "/etc/os-release" |
+                sed -E 's|.*="?([0-9]+\.[0-9]+).*|\1|')
+        elif grep -qE 'ID="?rhel' "/etc/os-release"; then
+            platformOSDist="redhat"
+            platformOSVersion=$(grep -m 1 "VERSION_ID=" "/etc/os-release" |
+                sed -E 's|.*="?([0-9]+\.[0-9]+).*|\1|')
+        else
+            die "Linux Distro '/etc/os-release' not supported currently:" \
+                "$(cat /etc/os-release 2>/dev/null)"
+        fi
+
+        # Remove whitespaces
+        platformOSDist="${platformOSDist// /}"
+        platformOSVersion="${platformOSVersion// /}"
+
+    elif [ "$_platformOS" = "darwin" ]; then
+
+        platformOSDist=$(sw_vers | grep -m 1 'ProductName' | sed -E 's/.*:\s+(.*)/\1/')
+        platformOSVersion=$(sw_vers | grep -m 1 'ProductVersion' | sed -E 's/.*([0-9]+\.[0-9]+\.[0-9]+)/\1/g')
+        # Remove whitespaces
+        platformOSDist="${platformOSDist// /}"
+        platformOSVersion="${platformOSVersion// /}"
+
+    elif [ "$_platformOS" = "windows" ]; then
+        platformOSVersion=$(systeminfo | grep -m 1 'OS Version:' | sed -E "s/.*:\s+([0-9]+\.[0-9]+\.[0-9]+).*/\1/")
+        platformOSVersion="${platformOSVersion// /}"
+    else
+        die "Platform '$_platformOS' not supported currently."
+    fi
+
+    if [ -n "${2:-}" ]; then
+        local -n _platformOSDist="$2"
+        _platformOSDist="$platformOSDist"
+    fi
+
+    if [ -n "${3:-}" ]; then
+        local -n _platformOSVersion="$3"
+        _platformOSVersion="$platformOSVersion"
+    fi
+
+    return 0
+}
+
+function getPlatformArch() {
+    local -n _arch="$1"
+
+    _arch=""
+    if uname -m | grep -q "x86_64" &>/dev/null; then
+        _arch="amd64"
+        return 0
+    elif uname -m | grep -q "aarch64" &>/dev/null; then
+        _arch="arm64"
+        return 0
+    elif uname -p | grep -q "arm64" &>/dev/null; then
+        _arch="arm64"
+    else
+        die "Architecture: '$(uname -m)' not supported."
+    fi
+}
 checkTool "jq"
 checkTool "curl"
 checkTool "sha256sum"
@@ -111,8 +208,11 @@ if ! versionCompare "${versionTag##v}" ">=" "2.3.4"; then
     exit 1
 fi
 
-systemName="$(uname | tr '[:upper:]' '[:lower:]')"
-systemArch="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/aarch64/arm64/')"
+os=""
+arch=""
+
+getPlatformOS os
+getPlatformArch arch
 
 # Download and install
 response=$(curl --silent --location "https://api.github.com/repos/$org/$repo/releases/tags/$versionTag") || {
@@ -123,14 +223,14 @@ response=$(curl --silent --location "https://api.github.com/repos/$org/$repo/rel
 checksumFileURL=$(echo "$response" | jq --raw-output ".assets[] | select( .name == \"githooks.checksums\") | .browser_download_url")
 
 url=$(echo "$response" |
-    jq --raw-output ".assets[] | select( (.name | contains(\"$systemName\")) and (.name | contains(\"$systemArch\")) ) | .browser_download_url") || {
+    jq --raw-output ".assets[] | select( (.name | contains(\"$os\")) and (.name | contains(\"$arch\")) ) | .browser_download_url") || {
     echo "Could not get assets from tag '$versionTag'."
     exit 1
 }
 
 if [ -z "$url" ]; then
-    echo -e "!! Unsupported operating system '$systemName' or \n" \
-        "machine type '$systemArch': \n" \
+    echo -e "!! Unsupported operating system '$os' or \n" \
+        "machine type '$arch': \n" \
         "Please check 'https://github.com/$org/${repo}/releases' manually."
 
     exit 1
@@ -147,7 +247,7 @@ githooks="$tempDir/githooks"
 mkdir -p "$githooks"
 
 cliExe="cli"
-if [ "$systemName" = "windows" ]; then
+if [ "$os" = "windows" ]; then
     cliExe="$cliExe.exe"
 fi
 
