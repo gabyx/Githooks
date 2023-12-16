23b44903dfc7bcb49d334e8e306e9aaed45d53db
Gabriel Nützi <gnuetzi@gmail.com>
2023-12-16 20:47:13 +0100

    fix: Tests :anchor:

--- tests/steps/step-138.sh
+++ tests/steps/step-138.sh
@@ -91,7 +91,8 @@ exitCode="$?"
 set -e
 
 # Coverage cannot report correct exit codes, without wrapping it to a file.
-if [ -z "$GH_COVERAGE_DIR" ] && [ "$exitCode" != "123" ]; then
+if [ -z "$GH_COVERAGE_DIR" ] &&
+    [ "$exitCode" != "123" ]; then
     echo "! Test script should have reported 123 [exit code: $exitCode]"
     exit 1
 fi
