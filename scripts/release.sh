61b613478bc1f10931fc57103f2e4667cfc35ab3
Gabriel Nützi <gnuetzi@gmail.com>
2024-01-03 10:37:14 +0100

    np: Fix tooling :anchor:

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-readarray -t prepareTag < <(git tag --pattern "prepare-*")
+readarray -t prepareTag < <(git tag --list "prepare-*")
 
 for tag in "${prepareTag[@]}"; do
     echo "Deleting prepare tag '$tag'."
@@ -11,9 +11,11 @@ done
 
 version="$1"
 tag="v$version"
-if git tag --pattern "v*" | grep -q "$tag"; then
+if git tag "v*" | grep -q "$tag"; then
     echo "Git tag '$tag' already exists."
 fi
 
 git tag "prepare-$tag"
+
+echo "Pushing tag 'prepare-$tag'."
 git push -f origin "prepare-$tag"
