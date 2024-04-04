5aa55d7db38d1c28d69f2c5b13630a8442e9e8a2
Gabriel Nützi <gnuetzi@gmail.com>
2024-04-04 16:41:48 +0200

    fix: Run wrapper

--- githooks/build/embedded/run-wrapper.sh
+++ githooks/build/embedded/run-wrapper.sh
@@ -4,15 +4,20 @@
 # It allows you to have a .githooks folder per-project that contains
 # its hooks to execute on various Git triggers.
 
-# Read the runner script from the local/global or system config
-GITHOOKS_RUNNER=$(git config githooks.runner)
+GITHOOKS_RUNNER=$(command -v "githooks-runner" >/dev/null 2>&1)
 
-if [ ! -x "$GITHOOKS_RUNNER" ]; then
-    echo "! Githooks runner points to a non existing location" >&2
-    echo "   \`$GITHOOKS_RUNNER\`" >&2
-    echo " or it is not executable!" >&2
-    echo " Please run the Githooks install script again to fix it." >&2
-    exit 1
+# shellcheck disable=SC2181
+if [ $? -ne 0 ]; then
+    GITHOOKS_RUNNER=$(git config githooks.runner)
+
+    if [ ! -x "$GITHOOKS_RUNNER" ]; then
+        echo "! Executable 'githooks-runner' is not in your path." >&2
+        echo "! Also the optional Git config value in 'githooks.runner' points to " >&2
+        echo "   \`$GITHOOKS_RUNNER\`" >&2
+        echo " which is not existing or it is not executable!" >&2
+        echo " Please run the Githooks install script again to fix it." >&2
+        exit 1
+    fi
 fi
 
 exec "$GITHOOKS_RUNNER" "$0" "$@"
